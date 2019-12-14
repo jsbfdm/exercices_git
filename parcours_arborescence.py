a8e6a96e54cde875f3a3bcf04e7be1c4fc235fa6
Paul <paul.roux12@hotmail.com>
2019-12-14 14:10:52 +0100

    Supression de la fonction est_un_repertoire et ajout d'une autre

--- parcours_arborescence.py
+++ parcours_arborescence.py
@@ -1,7 +1,15 @@
 import os
 
-def est_un_repertoire(rep) :
-    pass
-
+def existe_un_repertoire(rep) :
+    for i in os.rep :
+        if not os.path.isdir(rep) :
+            return False
+    return True
+    
+    
 def rechercher_fichiers(rep, nom_fichier) :
     pass
+
+
+if __name__ == "__main__" :
+    est_un_repertoire(".git")
