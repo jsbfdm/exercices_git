271466d2732f5f8f66bbabf1599a33d90223d13c
Paul <paul.roux12@hotmail.com>
2019-12-14 16:00:21 +0100

    Affiche le bon chemin mais dysfonctionnement quant à ce que retourne la fonction...

--- parcours_arborescence.py
+++ parcours_arborescence.py
@@ -1,15 +1,51 @@
 import os
+import glob
 
 def existe_un_repertoire(rep) :
-    for i in os.rep :
+    for i in os.listdir(rep) :
         if not os.path.isdir(rep) :
             return False
     return True
+
+def nb_rep(rep) :
+    nb = 0
+    for i in os.listdir(rep) :
+        if os.path.isdir(i) :
+            nb += 1
+    return nb
     
     
-def rechercher_fichiers(rep, nom_fichier) :
-    pass
+def rechercher_fichier(rep, nom_fichier) :
+    chemin = rep
+
+    for fichier in os.listdir(rep) :
+        
+        
+        if fichier == nom_fichier :
+            chemin = chemin + "/" + fichier
+            print("Trouvéééééééééééééééééééééééééééééééé", " ====== ", chemin)
+            return chemin
+            
+        else :     
+            fichier = chemin + "/" + fichier
+            
+            if os.path.isdir(fichier) :
+                
+                
+                rechercher_fichier(fichier, nom_fichier)
+
+                
+            
+                        
+                
+            
+            
+    
 
 
 if __name__ == "__main__" :
-    est_un_repertoire(".git")
+    existe_fichier = rechercher_fichier(".", "et voici le meilleur")
+    print(existe_fichier)
+ 
+    
+    
